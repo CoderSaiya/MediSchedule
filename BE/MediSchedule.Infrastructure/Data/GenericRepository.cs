283652903e5d1ebef17400a0a46100448e1d3c00
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-05-23 21:10:17 +0700

    implement basic CRUD

---
+++ BE/MediSchedule.Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq.Expressions;
+using MediSchedule.Domain.Interfaces;
+using MediSchedule.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediSchedule.Infrastructure.Data;
+
+public class GenericRepository<T>(AppDbContext context) : IRepository<T> where T : class
+{
+    public async Task<T?> GetByIdAsync(Guid id) => 
+        await context.Set<T>().FindAsync(id);
+
+    public async Task<IEnumerable<T>> ListAsync() => await context.Set<T>().ToListAsync();
+
+    public async Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate) =>
+        await context.Set<T>().Where(predicate).ToListAsync();
+
+
+    public async Task<IEnumerable<T>> ListAsync<TFilter>(TFilter filter)
+        where TFilter : class
+    {
+        IQueryable<T> query = context.Set<T>();
+        
+        foreach (var prop in typeof(TFilter).GetProperties())
+        {
+            var value = prop.GetValue(filter);
+            if (value == null) continue;
+
+
+            var entityProp = typeof(T).GetProperty(prop.Name);
+            if (entityProp == null) continue;
+            
+            var param = Expression.Parameter(typeof(T), "x");
+            var left = Expression.Property(param, entityProp);
+            var right = Expression.Constant(value);
+            var equal = Expression.Equal(left, right);
+            var lambda = Expression.Lambda<Func<T, bool>>(equal, param);
+
+            query = query.Where(lambda);
+        }
+
+        return await query.ToListAsync();
+    }
+
+    public async Task AddAsync(T entity) => await context.Set<T>().AddAsync(entity);
+
+    public Task UpdateAsync(T entity)
+    {
+        context.Set<T>().Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var e = await context.Set<T>().FindAsync(id);
+        if (e != null) context.Set<T>().Remove(e);
+    }
+}
