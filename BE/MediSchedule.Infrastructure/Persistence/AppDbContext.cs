ee5c5bf21564a625feead071164e19cf743ced3d
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-05-23 21:09:38 +0700

    add db context

---
+++ BE/MediSchedule.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using MediSchedule.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediSchedule.Infrastructure.Persistence;
+
+public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Profile> Profiles { get; set; }
+    public DbSet<Specialty> Specialties { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<ChatMessage> ChatMessages { get; set; }
+    public DbSet<ChatSession> ChatSessions { get; set; }
+    public DbSet<Slot> Slots { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
