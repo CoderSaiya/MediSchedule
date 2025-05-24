8088f1b6d2bf5544dcf294d812317a58df18a9c7
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-05-24 14:25:51 +0700

    implement mail service via SMTP

---
+++ BE/MediSchedule.Application/Interface/IMailService.cs
@@ -0,0 +1,6 @@
+﻿namespace MediSchedule.Application.Interface;
+
+public interface IMailService
+{
+    public Task SendEmailAsync(string toEmail, string subject, string body);
+}
--- BE/MediSchedule.Infrastructure/Configuration/DependencyInjection.cs
+++ BE/MediSchedule.Infrastructure/Configuration/DependencyInjection.cs
@@ -46,6 +46,7 @@ public static class DependencyInjection
        
        services.AddSingleton<IBlobService, AzureBlobService>();
        services.AddScoped<IAuthService, AuthService>();
+       services.AddScoped<IMailService, MailService>();
         
         return services;
     }
