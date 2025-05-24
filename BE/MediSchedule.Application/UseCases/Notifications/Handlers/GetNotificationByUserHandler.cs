055b4695edf3d31ae88de7254b01a342754fac09
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-05-24 22:41:21 +0700

    implement notification use case

---
+++ BE/MediSchedule.Application/UseCases/Notifications/Handlers/GetNotificationByUserHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using MediSchedule.Application.UseCases.Notifications.Queries;
+using MediSchedule.Domain.Entities;
+using MediSchedule.Domain.Interfaces;
+
+namespace MediSchedule.Application.UseCases.Notifications.Handlers;
+
+public class GetNotificationByUserHandler(INotificationRepository notificationRepository) : IRequestHandler<GetNotificationByUserQuery, IEnumerable<Notification>>
+{
+    public async Task<IEnumerable<Notification>> Handle(GetNotificationByUserQuery request, CancellationToken cancellationToken)
+        => await notificationRepository.GetByUserIdAsync(request.UserId);
+}
