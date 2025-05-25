0a30c759b3828d40fe43698658d2f5b1850ace25
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-05-25 08:13:22 +0700

    difine refresh token use case

---
+++ BE/MediSchedule.Application/UseCases/Authentication/Command/RefreshTokenCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace MediSchedule.Application.UseCases.Authentication.Command;
+
+public record RefreshTokenCommand(string RefreshToken) : IRequest<string>;
---
+++ BE/MediSchedule.Application/UseCases/Authentication/Handlers/RefreshTokenHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using MediSchedule.Application.Interface;
+using MediSchedule.Application.UseCases.Authentication.Command;
+
+namespace MediSchedule.Application.UseCases.Authentication.Handlers;
+
+public class RefreshTokenHandler(IAuthService authService) : IRequestHandler<RefreshTokenCommand, string>
+{
+    public async Task<string> Handle(RefreshTokenCommand request, CancellationToken cancellationToken)
+    {
+        var newToken = await authService.RefreshTokenAsync(request.RefreshToken);
+        if (newToken is null)
+            throw new Exception("Invalid refresh token");
+        
+        return newToken;
+    }
+}
