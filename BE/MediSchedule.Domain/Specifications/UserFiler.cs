f429842c555b9df7a92c7b8982f01be9e8a24a0d
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-05-26 10:40:07 +0700

    add user filer

---
+++ BE/MediSchedule.Domain/Specifications/UserFiler.cs
@@ -0,0 +1,10 @@
+﻿using MediSchedule.Domain.ValueObjects;
+
+namespace MediSchedule.Domain.Specifications;
+
+public class UserFiler
+{
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public UserStatus? Status { get; set; }
+}
