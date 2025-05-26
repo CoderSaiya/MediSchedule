412f51e2fb147c9456334d5e3e9203cb64bec2da
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-05-26 09:49:42 +0700

    add migartion fixes FK error in slot

---
+++ BE/MediSchedule.Infrastructure/Persistence/Migrations/20250526024757_FixFKDoctorInSlot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediSchedule.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixFKDoctorInSlot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
