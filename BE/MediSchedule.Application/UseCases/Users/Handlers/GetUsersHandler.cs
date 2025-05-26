a845142d6ad569596390815ebed409aa3504f0f2
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-05-26 10:39:51 +0700

    implement user use case

---
+++ BE/MediSchedule.Application/UseCases/Users/Handlers/GetUsersHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using MediSchedule.Application.UseCases.Users.Queries;
+using MediSchedule.Domain.Entities;
+using MediSchedule.Domain.Interfaces;
+
+namespace MediSchedule.Application.UseCases.Users.Handlers;
+
+public class GetUsersHandler(IUserRepository userRepository) : IRequestHandler<GetUsersQuery, IEnumerable<User>>
+{
+    public async Task<IEnumerable<User>> Handle(GetUsersQuery request, CancellationToken cancellationToken) =>
+        await userRepository.ListAsync(request.Filer);
+}
---
+++ BE/MediSchedule.Application/UseCases/Users/Queries/GetUsersQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using MediSchedule.Domain.Entities;
+using MediSchedule.Domain.Specifications;
+
+namespace MediSchedule.Application.UseCases.Users.Queries;
+
+public record GetUsersQuery(UserFiler Filer) : IRequest<IEnumerable<User>>;
