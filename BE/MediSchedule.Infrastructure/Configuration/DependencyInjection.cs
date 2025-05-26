ba9219961cadcbe1ece373e9fcaf6ff367415535
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-05-26 10:25:21 +0700

    Add missing controller service registration

--- BE/MediSchedule.Infrastructure/Configuration/DependencyInjection.cs
+++ BE/MediSchedule.Infrastructure/Configuration/DependencyInjection.cs
@@ -57,9 +57,9 @@ public static class DependencyInjection
     
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-        
         services.AddSignalR();
+        
+        services.AddControllers();
         return services;
     }
 }
