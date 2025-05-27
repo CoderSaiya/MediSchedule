5b7e938a16f2b540d4cada71781d222089735c2c
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-05-27 17:27:31 +0700

    add some ultis function (coverter amount, map icon, cn utils)

---
+++ fe/src/lib/icon.ts
@@ -0,0 +1,34 @@
+import {
+    Heart,
+    Brain,
+    Eye,
+    Bone,
+    Baby,
+    Stethoscope,
+    Clock,
+    Users,
+    Star,
+    ArrowRight,
+    CheckCircle,
+    TrendingUp,
+    Activity,
+    Zap,
+} from "lucide-react"
+import React from "react";
+
+export const iconMap: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
+    Heart,
+    Brain,
+    Eye,
+    Bone,
+    Baby,
+    Stethoscope,
+    Clock,
+    Users,
+    Star,
+    ArrowRight,
+    CheckCircle,
+    TrendingUp,
+    Activity,
+    Zap,
+}
