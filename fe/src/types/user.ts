7b545b5e3b237fee4d7ce6ea484c75b5605b4c79
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-05-31 13:26:49 +0700

    define doctor response

--- fe/src/types/user.ts
+++ fe/src/types/user.ts
@@ -2,3 +2,15 @@ export interface DoctorDto {
     id: string;
     name: string;
 }
+
+export interface Doctor {
+    id: string;
+    name: string;
+    levelEducation: string;
+    specialty: string;
+    experience: string;
+    rating: number;
+    reviews: number;
+    avatar: string;
+    available: boolean;
+}
