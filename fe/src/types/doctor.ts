63da3254afb53ea66632141985d1f99caa765e97
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-06-01 11:14:14 +0700

    add stats type for doctor page

---
+++ fe/src/types/doctor.ts
@@ -0,0 +1,8 @@
+export interface DashboardStats {
+    totalAppointmentsToday: number
+    appointmentsDelta: number
+    totalPendingPatients: number
+    completedAppointmentsToday: number
+    completedDelta: number
+    averageRating: number
+}
