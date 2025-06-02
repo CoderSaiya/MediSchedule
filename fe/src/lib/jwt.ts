860697f030e078f27c037028454ddfe9de66acd2
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-06-02 09:48:09 +0700

    fixes label

--- fe/src/lib/jwt.ts
+++ fe/src/lib/jwt.ts
@@ -1,15 +1,19 @@
 import {jwtDecode} from "jwt-decode";
 
 interface JWTPayload {
-    userId: string;
-    role: string;
+    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": string;
+    "http://schemas.microsoft.com/ws/2008/06/identity/claims/role": string;
 }
 
 export function decodeToken(token: string): { userId: string; role: string } {
     try {
         const decoded = jwtDecode<JWTPayload>(token);
-        const userId = decoded.userId;
-        const role = decoded.role;
+        const userId =
+            decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"] ||
+            "";
+        const role =
+            decoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"] ||
+            "";
         return { userId, role };
     } catch (err) {
         console.error("Giải mã token thất bại:", err);
