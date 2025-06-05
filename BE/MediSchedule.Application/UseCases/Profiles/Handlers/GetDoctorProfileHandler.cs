1c4883fe66f21254720af8d519248a5fdd4e6193
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-06-05 10:35:17 +0700

    implement doctor profile query

---
+++ BE/MediSchedule.Application/UseCases/Profiles/Handlers/GetDoctorProfileHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using MediatR;
+using MediSchedule.Application.DTOs;
+using MediSchedule.Application.UseCases.Profiles.Queries;
+using MediSchedule.Domain.Interfaces;
+
+namespace MediSchedule.Application.UseCases.Profiles.Handlers;
+
+public class GetDoctorProfileHandler(IDoctorRepository doctorRepository) : IRequestHandler<GetDoctorProfileQuery, DoctorProfileResponse>
+{
+    public async Task<DoctorProfileResponse> Handle(GetDoctorProfileQuery request, CancellationToken cancellationToken)
+    {
+        var doctor = await doctorRepository.GetByIdAsync(request.DoctorId);
+        
+        if (doctor is null)
+            return new DoctorProfileResponse(
+                Name: "",
+                Specialty: "",
+                Avatar: ""
+            );
+
+        return new DoctorProfileResponse(
+            Name: $"BS. {doctor.Profile!.FullName!}",
+            Specialty: $"Chuyên khoa {doctor.Specialty.Name}",
+            Avatar: doctor.Profile.AvatarUrl!
+        );
+    }
+}
---
+++ BE/MediSchedule.Application/UseCases/Profiles/Queries/GetDoctorProfileQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using MediSchedule.Application.DTOs;
+
+namespace MediSchedule.Application.UseCases.Profiles.Queries;
+
+public record GetDoctorProfileQuery(
+    Guid DoctorId
+    ) : IRequest<DoctorProfileResponse>;
