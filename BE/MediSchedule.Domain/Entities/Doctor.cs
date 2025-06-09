6d5b239f69cbaad5238095ba9ec92d76f4c70e26
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-06-09 08:31:00 +0700

    determine relationship with hospital

--- BE/MediSchedule.Domain/Entities/Doctor.cs
+++ BE/MediSchedule.Domain/Entities/Doctor.cs
@@ -7,6 +7,8 @@ public class Doctor : User
     public Guid SpecialtyId { get; set; }
     [ForeignKey("SpecialtyId")]
     public Specialty Specialty { get; set; } = null!;
+    public Guid? HospitalId { get; set; }
+    public Hospital? Hospital { get; set; }
 
     public string LicenseNumber { get; set; } = null!;
     public string Biography { get; set; } = null!;
