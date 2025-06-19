016e6210850eda770d2d92b5d7d4d68905fe23fc
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-06-19 19:20:18 +0700

    configure auth

--- BE/MediSchedule.API/Program.cs
+++ BE/MediSchedule.API/Program.cs
@@ -1,7 +1,11 @@
+using System.Text;
+using BE.Security;
 using MediSchedule.Application.UseCases.Authentication.Command;
 using MediSchedule.Domain.Common;
 using MediSchedule.Infrastructure.Configuration;
 using MediSchedule.Infrastructure.Hubs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -36,6 +40,24 @@ builder.Services.AddSwaggerGen(c =>
         }
     }));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -66,13 +88,23 @@ builder.Services.AddMediatR(cfg =>
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseMiddleware<RoleMappingMiddleware>();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
 app.UseCors();
 
-app.MapHub<NotificationHub>("/hubs/notifications");
+app.MapHub<NotificationHub>("/notificationhub");
 app.MapHub<ChatHub>("/chathub");
 
 // Configure the HTTP request pipeline.
