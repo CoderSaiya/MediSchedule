ccc924472ddb908cc87b020dc278d7a621fc1cfe
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-06-23 09:48:14 +0700

    provide cookies for client, handle logout endpoint

--- BE/MediSchedule.API/Controllers/AuthController.cs
+++ BE/MediSchedule.API/Controllers/AuthController.cs
@@ -32,12 +32,49 @@ public class AuthController(IMediator mediator) : Controller
         try
         {
             var result = await mediator.Send(new LoginCommand(request.Username, request.Password));
+            
+            var accessCookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = Request.IsHttps,
+                SameSite = SameSiteMode.None,
+                Path = "/",
+                Expires = DateTimeOffset.UtcNow.AddMinutes(60)
+            };
+            var refreshCookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = Request.IsHttps,
+                SameSite = SameSiteMode.None,
+                Path = "/",
+                Expires = DateTimeOffset.UtcNow.AddDays(7)
+            };
+            Response.Cookies.Append("accessToken", result.AccessToken, accessCookieOptions);
+            Response.Cookies.Append("refreshToken", result.RefreshToken, refreshCookieOptions);
+            
             return Ok(GlobalResponse<TokenResponse>.Success(result));
         }
         catch (Exception ex)
         {
             return StatusCode(500, GlobalResponse<string>.Error(ex.Message, 500));
         }
+    }
+    
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        var cookieOptions = new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = Request.IsHttps,
+            SameSite = SameSiteMode.None,
+            Path = "/",
+            Expires = DateTimeOffset.UtcNow.AddDays(-1)
+        };
+        Response.Cookies.Append("accessToken", "", cookieOptions);
+        Response.Cookies.Append("refreshToken", "", cookieOptions);
+        
+        return Ok(GlobalResponse<string>.Success("Đã đăng xuất"));
     }
     
     [HttpPost("refresh-token")]
