00669e92d38ba265266291d5e51add60794a6113
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-06-29 16:09:43 +0700

    override ListAsync method

--- BE/MediSchedule.Infrastructure/Persistence/Data/Repositories/NotificationRepository.cs
+++ BE/MediSchedule.Infrastructure/Persistence/Data/Repositories/NotificationRepository.cs
@@ -6,6 +6,14 @@ namespace MediSchedule.Infrastructure.Persistence.Data.Repositories;
 
 public class NotificationRepository(AppDbContext context) : GenericRepository<Notification>(context), INotificationRepository
 {
+    public override async Task<IEnumerable<Notification>> ListAsync()
+    {
+        return await context.Notifications
+            .Include(n => n.User)
+            .ThenInclude(u => u.Profile)
+            .ToListAsync();
+    }
+
     public async Task<IEnumerable<Notification>> GetByUserIdAsync(Guid userId)
         => await context.Notifications.Where(n => n.UserId == userId).ToListAsync();
 }
